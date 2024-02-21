0e93b94caa2bbc3e4f1f2a07b0a752ce60da12a9
Mhmd <Mohamed2015pre@gmail.com>
2024-02-21 18:49:21 +0300

    some edits and fixes

--- lib/main.dart
+++ lib/main.dart
@@ -136,15 +136,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// extension HexColor on Color {
-//   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-//   String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-//       '${alpha.toRadixString(16).padLeft(2, '0')}'
-//       '${red.toRadixString(16).padLeft(2, '0')}'
-//       '${green.toRadixString(16).padLeft(2, '0')}'
-//       '${blue.toRadixString(16).padLeft(2, '0')}';
-// }
-
 extension ColorToHex on Color {
   String get toHex {
     return "#${value.toRadixString(16).substring(2)}";
