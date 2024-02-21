6eb05fcbab0dd17d9f20a81283090a22328c120b
Mhmd <Mohamed2015pre@gmail.com>
2024-02-21 19:55:31 +0300

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prayer_counter/main.dart';
+import 'package:prayers_counters_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
